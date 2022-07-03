a6751fb3b6a08627862c6a42a319a92c238befd2
gastonmourelle <gastonmourelle99@gmail.com>
2022-07-03 02:18:31 -0300

    logs con filtro

--- logs_diarios.php
+++ logs_diarios.php
@@ -5,7 +5,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
-    <title>Logs Diarios</title>
+    <title>Logs</title>
     <?php
     session_start();
     include 'comp/head.php';
@@ -18,8 +18,43 @@
     include 'comp/menu.php';
     include 'config.php';
     ?>
-    <h1 class="display-6">Logs diarios</h1>
+    <h1 class="display-6">Logs</h1>
     <div class="btn-toolbar mb-2 mb-md-0">
+        <form action="" method="GET">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <input type="date" name="desde" class="form-control" value="
+                        <?php 
+                        if (isset($_GET['desde'])) {
+                            echo $_GET['desde'];
+                        }
+                        else{
+
+                        }
+                        ?>">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <input type="date" name="hasta" class="form-control" value="
+                        <?php 
+                        if (isset($_GET['hasta'])) {
+                            echo $_GET['hasta'];
+                        }
+                        else{
+
+                        }
+                        ?>">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-dark btn-sm">Filtrar</button>
+                    </div>
+                </div>
+            </div>
+        </form>
         <form action="excel.php" method="post">
             <button type="submit" name="descargar_excel" class="btn btn-outline-dark btn-sm">
                 <span data-feather="download-cloud"></span> Exportar XLS
@@ -44,38 +79,71 @@
             </thead>
             <tbody>
                 <?php
-                include 'db.php';
-                $pdo = Base::connect();
-                $tiempoActualUnix = time() - 10800;
-                $tiempoActual = gmdate('Y-m-d H:i:s', $tiempoActualUnix);
-                $diaActual = gmdate('Y-m-d', $tiempoActualUnix);
-                $sql = "SELECT * FROM logs WHERE horaSalida BETWEEN '$diaActual 00:00:01' AND '$diaActual 23:59:59' ORDER BY identificador DESC";
-                foreach ($pdo->query($sql) as $row) {
-                    $tiempoEntrada = strtotime($row['horaEntrada']);
-                    $tiempoSalida = strtotime($row['horaSalida']);
-                    $diferencia = ($tiempoSalida - $tiempoEntrada);
-                    $tiempoDiferencia = gmdate('H:i:s', $diferencia);
-                    echo '<tr style="vertical-align: middle;">';
-                    echo '<td><b>' . $row['identificador'] . '</b></a></td>';
-                    echo '<td><b>' . $row['nombre'] . '</b></a></td>';
-                    echo '<td>' . $tiempoDiferencia . '</td>';
-                    echo '<td>' . $row['horaEntrada'] . '</td>';
-                    echo '<td>' . $row['horaSalida'] . '</td>';
-                    echo '</tr>';
-                }
-                Base::disconnect();
+                if (isset($_GET['desde']) && isset($_GET['hasta'])) {
+                    $desde = $_GET['desde'];
+                    $hasta = $_GET['hasta'];
+
+                    $sql1 = "SELECT * FROM logs WHERE horaSalida BETWEEN '$desde' AND '$hasta' ORDER BY identificador DESC";
+                    $query1 = mysqli_query($conex, $sql1);
+
+                    if (mysqli_num_rows($query1) > 0) {
+                        foreach ($query1 as $row) {
+                            $tiempoEntrada = strtotime($row['horaEntrada']);
+                            $tiempoSalida = strtotime($row['horaSalida']);
+                            $diferencia = ($tiempoSalida - $tiempoEntrada);
+                            $tiempoDiferencia = gmdate('H:i:s', $diferencia);
                 ?>
+                            <tr style="vertical-align: middle;">
+                                <td><b><?php echo $row['identificador'] ?></b></a></td>
+                                <td><b><?php echo $row['nombre'] ?></b></a></td>
+                                <td><?php echo $tiempoDiferencia ?></td>
+                                <td><?php echo $row['horaEntrada'] ?></td>
+                                <td><?php echo $row['horaSalida'] ?></td>
+                            </tr>
+                        <?php
+                        }
+                    } else {
+                        echo "<h6>No se han encontrados resultados para tu búsqueda</h6>";
+                    }
+                } else {
+                    $tiempoActualUnix = time() - 10800;
+                    $tiempoActual = gmdate('Y-m-d H:i:s', $tiempoActualUnix);
+                    $diaActual = gmdate('Y-m-d', $tiempoActualUnix);
+
+                    $sql2 = "SELECT * FROM logs WHERE horaSalida BETWEEN '$diaActual 00:00:01' AND '$diaActual 23:59:59' ORDER BY identificador DESC";
+                    $query2 = mysqli_query($conex, $sql2);
+
+                    if (mysqli_num_rows($query2) > 0) {
+                        foreach ($query2 as $row) {
+                            $tiempoEntrada = strtotime($row['horaEntrada']);
+                            $tiempoSalida = strtotime($row['horaSalida']);
+                            $diferencia = ($tiempoSalida - $tiempoEntrada);
+                            $tiempoDiferencia = gmdate('H:i:s', $diferencia);
+                        ?>
+                            <tr style="vertical-align: middle;">
+                                <td><b><?php echo $row['identificador'] ?></b></a></td>
+                                <td><b><?php echo $row['nombre'] ?></b></a></td>
+                                <td><?php echo $tiempoDiferencia ?></td>
+                                <td><?php echo $row['horaEntrada'] ?></td>
+                                <td><?php echo $row['horaSalida'] ?></td>
+                            </tr>
+                <?php
+                        }
+                    }
+                }
+                ?>
+
         </table>
 
         <?php
         include 'comp/scripts.php';
         ?>
         <script type="text/javascript">
-      $(document).ready(function() {
+            $(document).ready(function() {
 
-        setTimeout(function() {
-          $(".alert").alert('close');
-        }, 4000);
-      });
-      </script>
+                setTimeout(function() {
+                    $(".alert").alert('close');
+                }, 4000);
+            });
+        </script>
 </body>
